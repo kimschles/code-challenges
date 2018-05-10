e0270076f9b27c1f412978e3d94292f710662351
coloradokim <schlesinger.kim@gmail.com>
2018-05-09 20:52:11 -0600

    update js solution and add ruby solution

--- 02_fizz_buzz/fizzbuzz.js
+++ 02_fizz_buzz/fizzbuzz.js
@@ -1,7 +1,7 @@
 const fizzBuzz = () => {
     resultArray = [];
     for (let i = 1; i <= 100; i++) 
-        if (i % 3 === 0 && i % 5 === 0) {
+        if (i % 15 === 0) {
             resultArray.push("fizzbuzz");
         }
     else if (i % 3 === 0) {
--- 02_fizz_buzz/fizzbuzz.rb
+++ 02_fizz_buzz/fizzbuzz.rb
@@ -1,3 +1,16 @@
 def fizzbuzz()
-
+	result_array = []
+	(1..100).each do |number| 
+		if (number % 15 === 0)
+			result_array << "fizzbuzz"
+		elsif (number % 3 === 0)
+			result_array << "fizz"
+		elsif (number % 5 === 0)
+			result_array << "buzz"
+		else 
+			result_array << number
+		end    
+	end  
+	return result_array 
 end 
+
